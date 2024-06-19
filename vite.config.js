2b62cd696ca02fdb76852c4a6f6da84c5f36a985
¨Julian <juliangarciam13@hotmail.com>
2024-06-19 13:34:09 -0500

    se modifico la base url para github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Ajustar la base URL según el nombre del repositorio
 export default defineConfig({
+  base: '/Portfolio/', // base URL para GitHub Pages
   plugins: [react()],
   build: {
     outDir: 'docs' // Configuración de salida para la carpeta docs
